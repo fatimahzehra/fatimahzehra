6f589ff7d163bf5fc8e895fbc9d01903b64173fe
fatimahzehra <137869258+fatimahzehra@users.noreply.github.com>
2023-07-12 23:06:38 +0500

    Add files via upload

---
+++ css2/code/index.cpp
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <navbar>
+    <img src="./img1.jpg" height="50px" alt="">
+    <div class="b">
+        <li class="a">Home</li>
+        <li>About</li>
+        <li>Blog</li>
+        <li>Contact</li>       
+ </div>
+    </navbar>  
+    <div class="t">
+        <img src="./img2.jpg" width="1540px" height="700px" alt="">
+
+    </div>  
+    <div>
+        <h2 class="x">NEW PRODUCT</h2>
+        <h1 class="c">The Twist OF Healthy Yogurt</h1>
+        <h4 class="k">This website template has been designed by</h4>
+        <h4 class="y"> <u>Free Website Templates</u></h4>
+        <h4 class="q"> for you, for free. You can replace all this text with your own text</h4>
+    </div>
+    <img src="./img4.jpg" height="181px" alt="">
+    <img src="./img5.jpg" height="181px" alt="">
+    <img src="./img6.jpg" height="181px" alt="">
+    <img src="./img7.jpg" height="181px" alt="">
+    
+
+
+</body>
+</html>
